16c1965156d85d81a381e46a995a3d0700e754ca
Sammot <45080494+Sammot@users.noreply.github.com>
2021-11-04 17:37:36 +0000

    Tweaked char[0] physics, added defense to attack manager

--- web/scripts/engine/attackManager.js
+++ web/scripts/engine/attackManager.js
@@ -4,6 +4,8 @@ function attackCheck(thisPlayer) {
   let throwVector = {x: 0, y: 0}
   let stun = 0
 
+  let thisPlayerDefense = thisPlayer.character.game.defence
+
   let thisPlayersHitbox = new Rectangle(thisPlayer.pos.x, thisPlayer.pos.y, thisPlayer.character.dimensions.width, thisPlayer.character.dimensions.height)
 
   for (let p in gameState.players) {
@@ -18,8 +20,8 @@ function attackCheck(thisPlayer) {
 
         if (collideRectRectObject(thisPlayersHitbox, theHitboxRectangle)) {
           doSound("punch")
-          totalDamage += theHitbox.move.damage
-          stun = theHitbox.move.stun
+          totalDamage += theHitbox.move.damage - thisPlayerDefense
+          stun = theHitbox.move.stun - Math.floor(thisPlayerDefense/2)
           throwVector.x = theHitbox.move.direction.x * PUNCHPOWER * theHitbox.move.direction.m * (1 + thisPlayer.damage/10)
           throwVector.y = theHitbox.move.direction.y * PUNCHPOWER * theHitbox.move.direction.m * (1 + thisPlayer.damage/10)
 
--- web/scripts/game/test.js
+++ web/scripts/game/test.js
@@ -2,5 +2,6 @@
 function gotoGame() {
   ScenesManager.scenes[CHARACTERSELECT].selection.player1 = 0
   ScenesManager.scenes[CHARACTERSELECT].selection.player2 = 0
-  ScenesManager.changeScene(LEVELSELECT, 0)
+  ScenesManager.scenes[LEVELSELECT].selection = 0
+  ScenesManager.changeScene(GAME, 0)
 }
