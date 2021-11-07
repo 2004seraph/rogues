e0f5922b0d5ed7b72f0bc182cb6372863b087028
Sammot <45080494+Sammot@users.noreply.github.com>
2021-11-07 19:28:02 +0000

    Tweaked char[0], made a proper todo list

--- web/scripts/engine/player.js
+++ web/scripts/engine/player.js
@@ -219,7 +219,7 @@ class Player {
       }
 
       //this weird if statement is meant to stop the players not colliding when the user tabs out of the game
-      if (!this.grounded) {
+      if (!this.grounded && this.launchVelocity.mag() < 1) {
         this.velocity.add(0, GRAVITY * this.character.physics.mass * timeScaler())//constantly pull the player down with gravity
       } else {
         this.velocity.add(0, 1)//to stop collide-uncollide loop
